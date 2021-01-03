386f7ab8fae1c00dc9ffe65ed674d509cf25d91d
seongahjo <seongside@gmail.com>
2021-01-03 17:48:00 +0900

    [#1] ApiResponseStatus Enum 추가

--- api-server/src/main/java/com/pinnochio/santaclothes/apiserver/dto/UploadResponseDto.java
+++ api-server/src/main/java/com/pinnochio/santaclothes/apiserver/dto/UploadResponseDto.java
@@ -1,10 +1,12 @@
 package com.pinnochio.santaclothes.apiserver.dto;
 
+import com.pinnochio.santaclothes.apiserver.type.ApiResponseStatus;
+
 import lombok.Value;
 
 @Value
 public class UploadResponseDto {
-	String status;
+	ApiResponseStatus status;
 
 	String message;
 }
---
+++ api-server/src/main/java/com/pinnochio/santaclothes/apiserver/type/ApiResponseStatus.java
@@ -0,0 +1,6 @@
+package com.pinnochio.santaclothes.apiserver.type;
+
+public enum ApiResponseStatus {
+	SUCCESS,
+	FAILED,
+}
