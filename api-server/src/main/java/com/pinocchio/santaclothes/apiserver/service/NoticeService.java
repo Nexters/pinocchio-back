1f2beab27fc7fd9c7283cca50073faa25cbe4789
seongahjo <seongside@gmail.com>
2021-02-11 01:11:33 +0900

    [#20] 공지사항 mock 수정

--- api-server/src/main/java/com/pinocchio/santaclothes/apiserver/service/NoticeService.java
+++ api-server/src/main/java/com/pinocchio/santaclothes/apiserver/service/NoticeService.java
@@ -17,27 +17,19 @@ public class NoticeService {
 	public List<Notice> findAllNotice(){
 		return List.of(
 			Notice.builder()
-				.title("라벨을 찍어보세요1")
+				.title("라벨을 찍어보세요")
 				.description("라벨을 찍고 세탁법을 확인해보세요1")
-				.content("라벨을 찍고 어떻게 확인하냐면 잘 해보세요1\n"
-					+ "라벨을 찍고 어떻게 확인하냐면 잘 해보세요1\n"
-					+ "라벨을 찍고 어떻게 확인하냐면 잘 해보세요1\n")
+				.content("라벨을 10개 찍으면 상품이 꽝!")
 				.build(),
 
 			Notice.builder()
-				.title("라벨을 찍어보세요2")
-				.description("라벨을 찍고 세탁법을 확인해보세요2")
-				.content("라벨을 찍고 어떻게 확인하냐면 잘 해보세요2\n"
-					+ "라벨을 찍고 어떻게 확인하냐면 잘 해보세요2\n"
-					+ "라벨을 찍고 어떻게 확인하냐면 잘 해보세요2\n")
+				.title("옷 세탁법이 궁금하세요?")
+				.description("카메라 버튼을 눌러 라벨을 찍어보세요")
 				.build(),
 
 			Notice.builder()
-				.title("라벨을 찍어보세요3")
-				.description("라벨을 찍고 세탁법을 확인해보세요3")
-				.content("라벨을 찍고 어떻게 확인하냐면 잘 해보세요3\n"
-					+ "라벨을 찍고 어떻게 확인하냐면 잘 해보세요3\n"
-					+ "라벨을 찍고 어떻게 확인하냐면 잘 해보세요3\n")
+				.title("찍힌 라벨이 맞는지 확인해주세요")
+				.description("수정 또는 인식이 안되었을 경우, 라벨 버튼을 꾹 눌러 수정해주세요")
 				.build()
 		);
 	}
